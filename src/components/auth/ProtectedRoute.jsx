0dba49446248876459635e63fcfd1bbc6610e8a6
Addomike23 <104534249+Addomike23@users.noreply.github.com>
2025-06-10 13:13:20 +0000

    Update ProtectedRoute.jsx

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useCart } from "../pages/context/Context";
 
 const ProtectedRoute = () => {
-    const {isLoggedIn}= useCart()
+    const { isLoggedIn } = useCart();
+    const location = useLocation();
 
-    return isLoggedIn? <Outlet/> : <Navigate to="/login" replace />;
+    return isLoggedIn ? (
+        <Outlet />
+    ) : (
+        <Navigate to="/login" replace state={{ from: location }} />
+    );
 };
 
 export default ProtectedRoute;
+
